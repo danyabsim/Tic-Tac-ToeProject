495ad920eb5246123a99274b31ed1a0694773938
Hadar <hadar5445@gmail.com>
2024-01-31 16:58:27 +0200

    updating the testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('This' , () => {
+  it('should play the game', () => {
+    render(<App />);
+    const firstPlayer = screen.getByTestId('firstPlayer');
+    const secondPlayer = screen.getByTestId('secondPlayer');
+    const submit = screen.getByTestId('submit');
+
+    fireEvent.input(firstPlayer, { target: {value: 'ABC'}});
+    fireEvent.input(secondPlayer, { target: {value: 'XYZ'}});
+    fireEvent.submit(submit);
+
+    expect(screen).toBeInTheDocument();
+  });
 });
