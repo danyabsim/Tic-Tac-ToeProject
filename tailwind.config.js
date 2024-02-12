208a76003c5f81863307a3e11fb89499f82049bf
Hadar <hadar5445@gmail.com>
2024-02-12 16:33:59 +0200

    adding tailwind option. @import 'tailwindcss/base';                         @import 'tailwindcss/components';                         @import 'tailwindcss/utilities';

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx', // Adjust the path based on your project structure
+    './src/**/*.ts',  // Adjust the path based on your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
