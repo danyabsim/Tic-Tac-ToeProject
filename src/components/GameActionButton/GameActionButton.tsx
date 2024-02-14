974381577068a4fd7ecbd65441c94fffb84b49bb
Hadar <hadar5445@gmail.com>
2024-02-14 12:49:20 +0200

    changing margin of buttons from 20 to 14 (10 => 7)

--- src/components/GameActionButton/GameActionButton.tsx
+++ src/components/GameActionButton/GameActionButton.tsx
@@ -2,7 +2,7 @@ import React, {HTMLProps} from "react";
 
 function GameActionButton(props: HTMLProps<HTMLInputElement>) {
     return (
-        <div className="border-2 border-black cursor-pointer ml-10 mr-10 inline-block mb-0 bg-green-500">
+        <div className="border-2 border-black cursor-pointer ml-7 mr-7 inline-block mb-0 bg-green-500">
             <input type="button"
                    value={props.value}
                    onClick={props.onClick}
