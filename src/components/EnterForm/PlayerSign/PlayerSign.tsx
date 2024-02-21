e0a2358014b24f3c5a8fa9bc029230f2fd6cf0d8
Hadar <hadar5445@gmail.com>
2024-02-21 16:21:25 +0200

    worked on some code improvements

--- src/components/EnterForm/PlayerSign/PlayerSign.tsx
+++ src/components/EnterForm/PlayerSign/PlayerSign.tsx
@@ -49,19 +49,13 @@ function PlayerSign(props: IPlayerSignProps) {
             <ToggleButton firstOption={firstOption} secondOption={secondOption}/>
             <label className="block mb-2">Your Player Sign:</label>
             {isCharChecked && (
-                <>
-                    <InputEnterForm value={props.currentPlayer.sign} maxLength={maxLengthOfSigns}
-                                    onChange={(event) => props.setCurrentPlayerSign(event.currentTarget.value)}/>
-                </>
+                <InputEnterForm value={props.currentPlayer.sign} maxLength={maxLengthOfSigns}
+                                onChange={(event) => props.setCurrentPlayerSign(event.currentTarget.value)}/>
             )}
             {isFileChecked && (
                 <div {...getRootProps()} className="bg-green-500 inline-block mt-4 mb-4 text-black">
                     <input {...getInputProps()} />
-                    {isDragActive ? (
-                        <p>Drop the files here...</p>
-                    ) : (
-                        <p>Drag 'n' drop some files here, or click to select files!</p>
-                    )}
+                    {isDragActive ? ("Drop the files here...") : ("Drag 'n' drop some files here, or click to select files!")}
                 </div>
             )}
         </>
