7d1bf4c4e472b637e411f0e138fc2602afedecc0
Hadar <hadar5445@gmail.com>
2024-02-22 16:43:28 +0200

    removing socket from redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
-import socketIOClient from 'socket.io-client';
 import historyReducer from './History/historySlice';
 import resultsReducer from "./Results/resultsSlice";
 import playersReducer from './Players/playersSlice';
@@ -15,21 +14,5 @@ export type RootState = ReturnType<typeof rootReducer>;
 const store = configureStore({
     reducer: rootReducer,
 });
-
-const socket = socketIOClient('http://localhost:4000');
-
-// Listen for state updates from the server
-socket.on('state', (newState) => {
-    store.dispatch({type: 'SET_STATE', payload: newState});
-});
-
-// Listen for actions from the server
-store.subscribe(() => {
-    const action = store.getState();
-    if (action) {
-        // Send the action to the server
-        socket.emit('action', action);
-    }
-})
 
 export default store;
